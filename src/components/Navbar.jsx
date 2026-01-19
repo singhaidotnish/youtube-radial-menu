f546a774fa367176b755c4ada0b43d7a3b63a4d0
zarir-engineer <zarir.utube@gmail.com>
2026-01-19 15:01:19 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,21 @@ import './Navbar.css';
 const Navbar = ({ is3D, onToggle, onAddClick }) => {
   return (
     <div className="navbar-container">
-      {/* 1. The Add Button */}
-      <button className="nav-add-btn" onClick={onAddClick} title="Add Link">
-        <Plus size={24} color="#fff" />
-      </button>
-
-      {/* 2. The 2D/3D Switch */}
-      {/* We wrap it to ensure layout stability */}
+      
+      {/* 1. LEFT SIDE: The 2D/3D Switch */}
       <div className="switch-wrapper">
         <ToggleSwitch is3D={is3D} onToggle={onToggle} />
       </div>
+
+      {/* 2. RIGHT SIDE: The Add Button */}
+      <button 
+        className="nav-add-btn" 
+        onClick={onAddClick} 
+        title="Add New Link"
+      >
+        <Plus size={24} color="#fff" />
+      </button>
+
     </div>
   );
 };
